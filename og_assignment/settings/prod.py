27d048cd70a2f810eed7a9b8f04199a92a0b818f
chanwoo7 <chanwoo9987@gmail.com>
2024-10-26 07:30:31 +0900

    [setting] 도메인 주소 등록

--- og_assignment/settings/prod.py
+++ og_assignment/settings/prod.py
@@ -1,6 +1,6 @@
 from .base import *
 
-ALLOWED_HOSTS = ['3.39.159.167']
+ALLOWED_HOSTS = ['3.39.159.167', 'opengalleryassignment.site']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = False
